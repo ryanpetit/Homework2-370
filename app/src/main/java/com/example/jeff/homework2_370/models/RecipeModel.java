1e6a975ad2e54656f1514f30b4dcdf025918a061
JustinR840 <ramosjus@sonoma.edu>
2017-03-13 14:39:35 -0700

    working code complete

--- app/src/main/java/com/example/jeff/homework2_370/models/RecipeModel.java
+++ app/src/main/java/com/example/jeff/homework2_370/models/RecipeModel.java
@@ -4,6 +4,25 @@ package com.example.jeff.homework2_370.models;
 public class RecipeModel {
     private String recipeName;
 
+    public String getRecipeImageUrl() {
+        return recipeImageUrl;
+    }
+
+    public void setRecipeImageUrl(String recipeImageUrl) {
+        this.recipeImageUrl = recipeImageUrl;
+    }
+
+    public String getRecipeDescription() {
+        return recipeDescription;
+    }
+
+    public void setRecipeDescription(String recipeDescription) {
+        this.recipeDescription = recipeDescription;
+    }
+
+    private String recipeImageUrl;
+    private String recipeDescription;
+
     public String getRecipeName() {
         return recipeName;
     }
