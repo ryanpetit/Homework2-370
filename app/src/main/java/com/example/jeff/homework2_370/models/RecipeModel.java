3eba6323e645d1591191f67bea0ad08510427608
ryyu <student@ssu-cs-dar28-167-215.sonoma.edu>
2017-03-20 21:17:48 -0700

    t working. Look at in the morning 3/20

--- app/src/main/java/com/example/jeff/homework2_370/models/RecipeModel.java
+++ app/src/main/java/com/example/jeff/homework2_370/models/RecipeModel.java
@@ -3,6 +3,8 @@ package com.example.jeff.homework2_370.models;
 
 public class RecipeModel {
     private String recipeName;
+    private String recipeImageURL;
+    private String recipeDescription;
 
     public String getRecipeName() {
         return recipeName;
@@ -10,5 +12,22 @@ public class RecipeModel {
 
     public void setRecipeName(String recipeName) {
         this.recipeName = recipeName;
+    }
+
+
+    public String getRecipeImageURL() {
+        return recipeImageURL;
+    }
+
+    public void setRecipeImageURL(String recipeImageURL) {
+        this.recipeImageURL = recipeImageURL;
+    }
+
+    public String getRecipeDescription() {
+        return recipeDescription;
+    }
+
+    public void setRecipeDescription(String recipeDescription) {
+        this.recipeDescription = recipeDescription;
     }
 }
--- app/src/main/java/com/example/jeff/homework2_370/utilities/RecipeParser.java
+++ app/src/main/java/com/example/jeff/homework2_370/utilities/RecipeParser.java
@@ -12,13 +12,19 @@ public class RecipeParser {
         RecipeModel model = null;
         try {
             JSONObject response = new JSONObject(jsonString);
+            // convert json string to a new JSON object.
             JSONArray matches = response.getJSONArray("matches");
             JSONObject recipe = matches.getJSONObject(0);
 
+            JSONArray images = response.getJSONArray("smallImageUrls");
+            JSONObject imageUrl = images.getJSONObject(0);
+
             model = new RecipeModel();
             model.setRecipeName(recipe.getString("recipeName"));
+            model.setRecipeImageURL(imageUrl.getString("smallImageUrls"));
+
         } catch (JSONException e) {
-            // do something useful with exception
+            System.out.println(e);
         }
         return model;
     }
